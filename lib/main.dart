936f91babf87098bc1ac2889bf0cab21765a7e11
ITadno <adrian@winau.one>
2021-12-01 14:12:07 +0100

    Home moved to extra file; widgets folder created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/start.dart';
-import 'pages/discover.dart';
-import 'pages/news.dart';
-import 'pages/settings.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,48 +28,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const Home(),
     );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<Widget> _pages = [Start(), Discover(), News(), Settings()];
-  int _selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).primaryColor,
-        backgroundColor: Theme.of(context).bottomAppBarColor,
-        type: BottomNavigationBarType.fixed,
-        onTap: _onItemTapped,
-        currentIndex: _selectedIndex,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Start"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Discover"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: "News"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
-        ],
-      ),
-    );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
