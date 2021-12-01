708dd301b020aae11b116485a91920aa1cd33a73
Tassianna <tasi.dafn@gmail.com>
2021-12-01 20:00:15 +0100

    Basic View for Settings Page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,4 +1,7 @@
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:swp_direktdem_verf_app/widgets/settingsbuttom.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -11,9 +14,33 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
-      body: Center(
-        child: Text("Hallo das ist die Seite Settings"),
+      appBar: AppBar(title: const Text('Settings')),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
+        child: ListView(
+          children: [
+            SettingsButton(Icons.notes, "Datenschutzbestimmungen", () {}),
+            const Divider(
+              color: Colors.purple,
+            ),
+            SettingsButton(CommunityMaterialIcons.account_edit,
+                "Profileinstellungen", () {}),
+            const Divider(
+              color: Colors.purple,
+            ),
+            SettingsButton(Icons.create, "Neues Erfahren erstellen", () {}),
+            const Divider(
+              color: Colors.purple,
+            ),
+            SettingsButton(Icons.question_answer, "FAQ", () {}),
+            const Divider(
+              color: Colors.purple,
+            ),
+            SettingsButton(
+                CommunityMaterialIcons.logout_variant, "Ausloggen", () {}),
+          ],
+        ),
       ),
     );
   }
