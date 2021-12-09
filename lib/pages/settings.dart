113fc78600e619922cbd8c569f004ced44079aec
Nata <nkononenko@zedat.fu-berlin.de>
2021-12-09 20:04:49 +0100

    adding Datenschutzbestimmungen adjusting pubspec.yaml to make it possible to add assets adjusting settings to navigate Datenschutybestimmungen

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,9 +1,10 @@
 import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:swp_direktdem_verf_app/pages/datasecurity.dart';
 import 'package:swp_direktdem_verf_app/pages/logout.dart';
-import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
-import 'package:swp_direktdem_verf_app/widgets/custom_bottom_navigation_bar.dart';
 import 'package:swp_direktdem_verf_app/widgets/settingsbuttom.dart';
+import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -17,45 +18,40 @@ class _SettingsState extends State<Settings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar('Settings'),
-      bottomNavigationBar: const CustomBottomNavigationBar(selectedIndex: 3),
       body: Container(
         width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
         child: ListView(
           children: [
-            SettingsButton(Icons.notes, 'Datenschutzbestimmungen', () {}),
+            SettingsButton(Icons.notes, "Datenschutzbestimmungen", () {
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DataSecurity()),
+            );
+            }),
             const Divider(
               color: Colors.purple,
             ),
-            SettingsButton(
-              CommunityMaterialIcons.account_edit,
-              'Profileinstellungen',
-              () {},
-            ),
+            SettingsButton(CommunityMaterialIcons.account_edit,
+                "Profileinstellungen", () {}),
             const Divider(
               color: Colors.purple,
             ),
-            SettingsButton(
-              CommunityMaterialIcons.archive_outline,
-              'Meine Verfahren',
-              () {},
-            ),
+            SettingsButton(CommunityMaterialIcons.archive_outline,
+                "Meine Verfahren", () {}),
             const Divider(
               color: Colors.purple,
             ),
-            SettingsButton(
-              Icons.admin_panel_settings_outlined,
-              'Benutzerverwaltung',
-              () {},
-            ),
+            SettingsButton(Icons.admin_panel_settings_outlined,
+                "Benutzerverwaltung", () {}),
             const Divider(
               color: Colors.purple,
             ),
-            SettingsButton(CommunityMaterialIcons.logout_variant, 'Ausloggen',
+            SettingsButton(CommunityMaterialIcons.logout_variant, "Ausloggen",
                 () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const Logout()),
+                MaterialPageRoute(builder: (context) => Logout()),
               );
             }),
           ],
