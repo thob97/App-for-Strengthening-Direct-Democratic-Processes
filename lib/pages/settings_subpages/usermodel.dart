9a30200511f9fb0e0489d33d2b248665506a1e01
Nata <nkononenko@zedat.fu-berlin.de>
2021-12-20 15:07:17 +0100

    adding user class to use data per user

---
+++ lib/pages/settings_subpages/usermodel.dart
@@ -0,0 +1,13 @@
+class User {
+  final int index;
+  final String name;
+  final String email;
+  final String password;
+  final String picture;
+  const User(
+      {required this.index,
+      required this.name,
+      required this.email,
+      required this.password,
+      required this.picture});
+}
