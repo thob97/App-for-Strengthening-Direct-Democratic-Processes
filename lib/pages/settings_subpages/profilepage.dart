9f3cd865b51a3f25dbd157d9ec5d228079d92977
Nata <nkononenko@zedat.fu-berlin.de>
2021-12-21 16:08:59 +0100

    adding profilepage and profile widget

---
+++ lib/pages/settings_subpages/profilepage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:swp_direktdem_verf_app/pages/settings_subpages/profilesettings.dart';
+import 'package:swp_direktdem_verf_app/pages/settings_subpages/usermodel.dart';
+import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
+import 'package:swp_direktdem_verf_app/widgets/profilewidget.dart';
+
+class ProfilePage extends StatelessWidget {
+  final User user;
+  const ProfilePage(this.user, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // const user = UserPreferences.myUser;
+
+    return Container(
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: const CustomAppBar('Profil'),
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                imagePath: user.picture,
+                onClicked: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => EditProfilePage(user)),
+                  );
+                },
+              ),
+              const SizedBox(height: 24),
+              buildName(user),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name.split(" ").first,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.name.split(" ").last,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.email,
+            style: const TextStyle(color: Colors.grey),
+          )
+        ],
+      );
+}
