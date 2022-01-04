5dfd3e50bfd9d8081534b55100f286c72aeec0fd
thob97 <thob97@mi.fu-berlin.de>
2022-01-04 20:51:51 +0100

    feat: add custom card widget
     add a card widget which looks like the card widget from flutter, but it prevents ripple / splash effects to overflow over the cards edges

---
+++ lib/widgets/selected_process/custom_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:swp_direktdem_verf_app/config/custom_theme_data.dart';
+
+//Looks like card, it prevents ripple / splash effects
+// to overflow over the cards edges
+class CustomCard extends StatelessWidget {
+  const CustomCard({required this.child});
+
+  final Widget child;
+
+  ///Style
+  static const double _horPad = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: _horPad),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(MyConstants.borderRadius),
+        child: Material(
+          color: Theme.of(context).cardTheme.color,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
