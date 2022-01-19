52d707c190597985978dbcaf9b883a5b1785e68b
Nata <nkononenko@zedat.fu-berlin.de>
2022-01-19 22:51:10 +0100

    feat: - creating class UserScreen

---
+++ lib/pages/settings_subpages/usermanagement.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:swp_direktdem_verf_app/pages/settings_subpages/profilepage.dart';
+import 'package:swp_direktdem_verf_app/pages/settings_subpages/usermodel.dart';
+import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
+
+class UserScreen extends StatefulWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserScreenState createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  TextEditingController controller = TextEditingController();
+
+  // Get json result and convert it to model. Then add
+  Future<void> getUserDetails() async {
+    final response = await http.get(Uri.parse(url));
+    final responseJson = json.decode(response.body);
+
+    setState(() {
+      for (final Map user in responseJson) {
+        _userDetails.add(User.fromJson(user.cast()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDetails();
+    _userDetails.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar('Benutzerverwaltung'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.search),
+                  title: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                        hintText: 'Suche', border: InputBorder.none,),
+                    onChanged: onSearchTextChanged,
+                  ),
+                  trailing: IconButton(icon: const Icon(Icons.cancel), onPressed: () {
+                    controller.clear();
+                    onSearchTextChanged('');
+                  },),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child:  _searchResult.isNotEmpty || controller.text.isNotEmpty
+                ? ListView.builder(
+              itemCount: _searchResult.length,
+              itemBuilder: (context, i) {
+                return Card(
+                  margin: const EdgeInsets.all(0.0),
+                  child: ListTile(
+                    leading: CircleAvatar(backgroundImage: NetworkImage(_searchResult[i].imagePath,),),
+                    title: Text('${_searchResult[i].name.split(" ").first} ${_searchResult[i].name.split(" ").last}'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ProfilePage(_searchResult[i]),),);
+                    },
+                  ),
+                );
+              },
+            )
+                : ListView.builder(
+              itemCount: _userDetails.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: const EdgeInsets.all(0.0),
+                  child: ListTile(
+                    leading: CircleAvatar(backgroundImage: NetworkImage(_userDetails[index].imagePath,),),
+                    title: Text('${_userDetails[index].name.split(" ").first} ${_userDetails[index].name.split(" ").last}'),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ProfilePage(_userDetails[index]),),);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> onSearchTextChanged(String text) async {
+    _searchResult.clear();
+    if (text.isEmpty) {
+      setState(() {});
+      return;
+    }
+
+    for (final userDetail in _userDetails) {
+      if (userDetail.name.split(' ').first.contains(text) || userDetail.name.split(' ').last.contains(text)) {
+        _searchResult.add(userDetail);
+      }
+    }
+    setState(() {});
+  }
+}
+
+List<User> _searchResult = [];
+
+List<User> _userDetails = [];
+
+const String url = 'https://jsonplaceholder.typicode.com/users';
