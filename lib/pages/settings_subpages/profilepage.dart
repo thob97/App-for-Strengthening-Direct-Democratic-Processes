35131ab757701074e8463b279739d14272f8b04c
Nata <nkononenko@zedat.fu-berlin.de>
2022-01-19 22:47:48 +0100

    feat: - creating class ProfilePage

--- lib/pages/settings_subpages/profilepage.dart
+++ lib/pages/settings_subpages/profilepage.dart
@@ -1,144 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:swp_direktdem_verf_app/config/route_generator.dart';
 import 'package:swp_direktdem_verf_app/pages/settings_subpages/profilesettings.dart';
 import 'package:swp_direktdem_verf_app/pages/settings_subpages/usermodel.dart';
 import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
-import 'package:swp_direktdem_verf_app/widgets/two_butts_in_row.dart';
+import 'package:swp_direktdem_verf_app/widgets/profilewidget.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage(this.user, {Key? key}) : super(key: key);
   final User user;
-
+  const ProfilePage(this.user, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // const user = UserPreferences.myUser;
-    return Builder(
-      builder: (context) => Scaffold(
-        appBar: const CustomAppBar('Profil'),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 24),
-                buildName(user, context),
-                const SizedBox(height: 35),
-                TwoButtInRow(
-                  functionOne: () => _showMyDialog(context),
-                  functionTwo: () async {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => EditProfilePage(user),
-                      ),
-                    );
-                  },
-                  buttonNameTwo: 'Profil bearbeiten',
-                  buttonNameOne: 'Ausloggen',
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 
-  Widget buildName(User user, BuildContext context) {
-    const bool isSwitched = true;
-    return Column(
-      children: [
-        Text(
-          user.name.split(' ').first,
-          style: Theme.of(context).textTheme.headline2,
-        ),
-        const SizedBox(height: 4),
-        Text(
-          user.name.split(' ').last,
-          style: Theme.of(context).textTheme.headline2,
-        ),
-        const SizedBox(height: 4),
-        Text(
-          user.email,
-          style: Theme.of(context).textTheme.bodyText2,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'Email Adresse sichtbar machen',
-              style: Theme.of(context).textTheme.bodyText2,
-            ),
-            Switch(
-              value: isSwitched,
-              onChanged: (value) {},
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-      ],
-    );
-  }
-
-  Future<void> _showMyDialog(BuildContext context) async {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(
-            'Ausloggen',
-            style: Theme.of(context)
-                .textTheme
-                .bodyText1
-                ?.apply(color: Theme.of(context).colorScheme.primary),
-          ),
-          content: Text(
-            'Möchten Sie Ihre Sitzung wirklich beenden?',
-            style: Theme.of(context)
-                .textTheme
-                .bodyText2
-                ?.apply(color: Theme.of(context).colorScheme.primary),
-          ),
-          actions: <Widget>[
-            _cancelButton(context),
-            _confirmButton(context),
-          ],
-          elevation: 24.0,
-          backgroundColor: Theme.of(context).colorScheme.background,
-        );
-      },
-    );
-  }
-
-  Widget _cancelButton(BuildContext context) {
-    return TextButton(
-      onPressed: () => Navigator.pop(context, 'Abbrechen'),
-      style: ButtonStyle(
-        textStyle: MaterialStateProperty.all(
-          Theme.of(context).textTheme.bodyText2?.apply(
-                color: Theme.of(context).colorScheme.onBackground,
+    return Container(
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: const CustomAppBar('Profil'),
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                imagePath: user.imagePath,
+                onClicked: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => EditProfilePage(user),),
+                  );
+                },
               ),
+              const SizedBox(height: 24),
+              buildName(user),
+            ],
+          ),
         ),
       ),
-      child: const Text('Abbrechen'),
     );
   }
 
-  Widget _confirmButton(BuildContext context) {
-    return TextButton(
-      onPressed: () => Navigator.of(context).pushNamed(
-        '/home',
-        arguments: const NavigationArguments(),
-      ), //todo push to login view
-      style: ButtonStyle(
-        textStyle: MaterialStateProperty.all(
-          Theme.of(context).textTheme.bodyText2?.apply(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-        ),
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.name.split(' ').first,
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
       ),
-      child: const Text('Ja'),
-    );
-  }
+      const SizedBox(height: 4),
+      Text(
+        user.name.split(' ').last,
+        style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      ),
+      const SizedBox(height: 4),
+      Text(
+        user.email,
+        style: const TextStyle(color: Colors.grey),
+      )
+    ],
+  );
 }
