d6b2711ead1ea46f6d610d4f3b4fd5d177051d86
Nata <nkononenko@zedat.fu-berlin.de>
2022-01-19 22:47:29 +0100

    feat: - creating class EditProfilePage

--- lib/pages/settings_subpages/profilesettings.dart
+++ lib/pages/settings_subpages/profilesettings.dart
@@ -1,157 +1,177 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:swp_direktdem_verf_app/pages/settings_subpages/change_password.dart';
-import 'package:swp_direktdem_verf_app/pages/settings_subpages/profilepage.dart';
 import 'package:swp_direktdem_verf_app/pages/settings_subpages/usermodel.dart';
-import 'package:swp_direktdem_verf_app/utils/user_preferences.dart';
 import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
-import 'package:swp_direktdem_verf_app/widgets/textfield_login_register.dart';
-import 'package:swp_direktdem_verf_app/widgets/two_butts_in_row.dart';
+import 'package:swp_direktdem_verf_app/widgets/profilewidget.dart';
 
-class EditProfilePage extends StatefulWidget {
-  const EditProfilePage(this.user, {Key? key}) : super(key: key);
+class EditProfilePage extends StatelessWidget {
   final User user;
-
-  @override
-  _EditProfilePageState createState() => _EditProfilePageState();
-}
-
-class _EditProfilePageState extends State<EditProfilePage> {
-  _EditProfilePageState();
-
-  final _formkey = GlobalKey<FormState>();
-  final controllerFirstName = TextEditingController();
-  final controllerLastName = TextEditingController();
-  final controllerEmail = TextEditingController();
-  final controllerPassword = TextEditingController();
-  bool isSwitched = false;
-
-  @override
-  void dispose() {
-    controllerFirstName.dispose();
-    controllerLastName.dispose();
-    controllerEmail.dispose();
-    controllerPassword.dispose();
-    super.dispose();
-  }
+  const EditProfilePage(this.user, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const bool switchValue = false;
     return Scaffold(
       appBar: const CustomAppBar('Profileinstellungen'),
-      body: SingleChildScrollView(
+      body: Container(
         padding: const EdgeInsets.only(top: 16.0),
-        child: Center(
-          child: Form(
-            key: _formkey,
-            child: Column(
+        child: ListView(
+          children: [
+            ProfileWidget(
+              imagePath: user.imagePath,
+              isEdit: true,
+              onClicked: () async {},
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            buildTextField('Vorname', user.name.split(' ').first),
+            buildTextField('Nachname', user.name.split(' ').last),
+            buildTextField('Email', user.email),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                TextfieldLoginRegister(
-                  'Vorname',
-                  UserPreferences.myUser.name.split(' ').first,
-                  controllerFirstName,
-                ),
-                TextfieldLoginRegister(
-                  'Nachname',
-                  UserPreferences.myUser.name.split(' ').last,
-                  controllerLastName,
-                ),
-                TextfieldLoginRegister(
-                  'Email',
-                  UserPreferences.myUser.email,
-                  controllerEmail,
-                ),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            const ChangePasswordPage(UserPreferences.myUser),
-                      ),
-                    );
-                  },
-                  style: Theme.of(context).elevatedButtonTheme.style,
-                  child: const Text(
-                    'PASSWORT ÄNDERN',
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'Email Adresse sichtbar machen',
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
-                    Switch(
-                      onChanged: (bool value) {
-                        if (isSwitched == false) {
-                          setState(() {
-                            isSwitched = true;
-                          });
-                        } else {
-                          setState(() {
-                            isSwitched = false;
-                          });
-                        }
-                      },
-                      value: isSwitched,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TwoButtInRow(
-                  buttonNameOne: 'Abbrechen',
-                  buttonNameTwo: 'Speichern',
-                  functionOne: () async {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            const ProfilePage(UserPreferences.myUser),
-                      ),
-                    );
-                  },
-                  functionTwo: () async {
-                    final form = _formkey.currentState!;
-
-                    if (form.validate()) {
-                      TextInput.finishAutofillContext();
-
-                      ScaffoldMessenger.of(context)
-                        ..removeCurrentSnackBar()
-                        ..showSnackBar(
-                          const SnackBar(
-                            content: Text('Ihre Daten werden gespeichert'),
-                          ),
-                        );
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              const ProfilePage(UserPreferences.myUser),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context)
-                        ..removeCurrentSnackBar()
-                        ..showSnackBar(
-                          const SnackBar(
-                            content: Text(
-                              'Ihre Daten können nicht gespeichert werden. Überprüfen Sie sie',
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),),
+                            elevation: 16,
+                            child: Container(
+                              height: 400.0,
+                              width: 360.0,
+                              child: ListView(
+                                children: <Widget>[
+                                  Center(
+                                    child: Text(
+                                      'Passwort ändern',
+                                      style: const TextTheme().headline2,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  buildTextField(
+                                      'Altes Passwort', 'Altes Passwort',),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    'Neues Passwort',
+                                    style: const TextTheme().headline2,
+                                  ),
+                                  buildTextField('Passwort', 'Passwort'),
+                                  buildTextField('Passwort wiederholen',
+                                      'Passwort wiederholen',),
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      primary: const ColorScheme.light().primary,
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 50,),
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                          BorderRadius.circular(20),),),
+                                    child: Text(
+                                      'Speichern',
+                                      style: const TextTheme().bodyText2,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                    }
+                          );
+                        },);
                   },
-                ),
+                  style: ElevatedButton.styleFrom(
+                      primary: const ColorScheme.light().primary,
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),),),
+                  child: Text(
+                    'PASSWORT ÄNDERN',
+                    style: const TextTheme().bodyText2,
+                  ),
+                )
               ],
             ),
-          ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Text(
+                'Email Adresse sichtbar machen',
+                style: const TextTheme().bodyText2,
+              ),
+              Transform.scale(
+                  scale: 0.7,
+                  child: Switch(
+                    value: switchValue,
+                    onChanged: (value) {},
+                  ),)
+            ],),
+            const SizedBox(
+              height: 35,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: const ColorScheme.light().primary,
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),),),
+                  child: Text(
+                    'ABBRECHEN',
+                    style: const TextTheme().bodyText2,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: const ColorScheme.light().primary,
+                      padding: const EdgeInsets.symmetric(horizontal: 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),),),
+                  child: Text(
+                    'SPEICHERN',
+                    style: const TextTheme().bodyText2,
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Widget buildTextField(String labelText, String placeholder) {
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.only(
+          bottom: 35.0,
+        ),
+        child: SizedBox(
+          height: 60,
+          width: 350,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              autofocus: false,
+              onTap: () {},
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(width: 2),
+                ),
+                contentPadding: const EdgeInsets.only(bottom: 3),
+                labelText: '  $labelText',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                hintText: '  $placeholder',
+                labelStyle: const TextTheme().headline2,
+                hintStyle: const TextTheme().headline2,
+              ),
+            ),
+          ),
+        ),
+      )
+    ],);
   }
 }
