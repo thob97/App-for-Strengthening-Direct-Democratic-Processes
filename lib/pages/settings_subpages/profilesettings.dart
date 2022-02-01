65fb89b03cedfe648ee145923b51d53ff7b4017d
Nata <nkononenko@zedat.fu-berlin.de>
2022-02-01 15:33:18 +0100

    fix: correction of SingleChildScrollView(Column) in ProfilePage and EditProfilePage; reformat of codes

--- lib/pages/settings_subpages/profilesettings.dart
+++ lib/pages/settings_subpages/profilesettings.dart
@@ -14,167 +14,164 @@ class EditProfilePage extends StatelessWidget {
     const bool switchValue = false;
     return Scaffold(
       appBar: const CustomAppBar('Profileinstellungen'),
-      body: Container(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.only(top: 16.0),
-        child: ListView(
-          children: [
-            ProfileWidget(
-              imagePath: user.imagePath,
-              isEdit: true,
-              onClicked: () async {},
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            TextfieldLoginRegister('Vorname', user.name.split(' ').first),
-            TextfieldLoginRegister('Nachname', user.name.split(' ').last),
-            TextfieldLoginRegister('Email', user.email),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Dialog(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.background,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          elevation: 16,
-                          child: SizedBox(
-                            height: 500.0,
-                            width: 400.0,
-                            child: ListView(
-                              padding: const EdgeInsets.all(10.0),
-                              children: <Widget>[
-                                IconButton(
-                                  icon: const Icon(Icons.cancel),
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            const EditProfilePage(
-                                          UserPreferences.myUser,
-                                        ),
+        child: Center(
+          child: Column(
+            children: [
+              ProfileWidget(
+                imagePath: user.imagePath,
+                isEdit: true,
+                onClicked: () async {},
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              TextfieldLoginRegister('Vorname', user.name.split(' ').first),
+              TextfieldLoginRegister('Nachname', user.name.split(' ').last),
+              TextfieldLoginRegister('Email', user.email),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.background,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 16,
+                        child: SizedBox(
+                          height: 500.0,
+                          width: 400.0,
+                          child: ListView(
+                            padding: const EdgeInsets.all(10.0),
+                            children: <Widget>[
+                              IconButton(
+                                icon: const Icon(Icons.cancel),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const EditProfilePage(
+                                        UserPreferences.myUser,
                                       ),
-                                    );
-                                  },
-                                  alignment: Alignment.centerRight,
-                                ),
-                                Center(
-                                  child: Text(
-                                    'Passwort ändern',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline2
-                                        ?.apply(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                        ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                const TextfieldLoginRegister(
-                                  'Altes Passwort',
-                                  'Altes Passwort',
-                                ),
-                                const SizedBox(height: 10),
-                                Center(
-                                  child: Text(
-                                    'Neues Passwort',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline2
-                                        ?.apply(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                        ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                const TextfieldLoginRegister(
-                                  'Passwort',
-                                  'Passwort',
-                                ),
-                                const TextfieldLoginRegister(
-                                  'Passwort wiederholen',
-                                  'Passwort wiederholen',
-                                ),
-                                Align(
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: Theme.of(context)
-                                        .elevatedButtonTheme
-                                        .style,
-                                    child: Text(
-                                      'Speichern',
-                                      style:
-                                          Theme.of(context).textTheme.bodyText2,
                                     ),
+                                  );
+                                },
+                                alignment: Alignment.centerRight,
+                              ),
+                              Center(
+                                child: Text(
+                                  'Passwort ändern',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline2
+                                      ?.apply(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              const TextfieldLoginRegister(
+                                'Altes Passwort',
+                                'Altes Passwort',
+                              ),
+                              const SizedBox(height: 10),
+                              Center(
+                                child: Text(
+                                  'Neues Passwort',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline2
+                                      ?.apply(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              const TextfieldLoginRegister(
+                                'Passwort',
+                                'Passwort',
+                              ),
+                              const TextfieldLoginRegister(
+                                'Passwort wiederholen',
+                                'Passwort wiederholen',
+                              ),
+                              Align(
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: Theme.of(context)
+                                      .elevatedButtonTheme
+                                      .style,
+                                  child: Text(
+                                    'Speichern',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2,
                                   ),
                                 ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                        );
-                      },
-                    );
-                  },
-                  style: Theme.of(context).elevatedButtonTheme.style,
-                  child: Text(
-                    'PASSWORT ÄNDERN',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  'Email Adresse sichtbar machen',
+                        ),
+                      );
+                    },
+                  );
+                },
+                style: Theme.of(context).elevatedButtonTheme.style,
+                child: Text(
+                  'PASSWORT ÄNDERN',
                   style: Theme.of(context).textTheme.bodyText2,
                 ),
-                Transform.scale(
-                  scale: 0.7,
-                  child: Switch(
-                    value: switchValue,
-                    onChanged: (value) {},
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 35,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: Theme.of(context).elevatedButtonTheme.style,
-                  child: Text(
-                    'ABBRECHEN',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'Email Adresse sichtbar machen',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: Theme.of(context).elevatedButtonTheme.style,
-                  child: Text(
-                    'SPEICHERN',
-                    style: Theme.of(context).textTheme.bodyText2,
+                  Transform.scale(
+                    scale: 0.7,
+                    child: Switch(
+                      value: switchValue,
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: Theme.of(context).elevatedButtonTheme.style,
+                    child: Text(
+                      'ABBRECHEN',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
                   ),
-                )
-              ],
-            )
-          ],
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: Theme.of(context).elevatedButtonTheme.style,
+                    child: Text(
+                      'SPEICHERN',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
