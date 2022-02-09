d60ecb63cabc6fe199aad58286597ed68623f8e9
Nata <nkononenko@zedat.fu-berlin.de>
2022-02-09 03:15:46 +0100

    fix: remove imge widget as it is not used

--- lib/pages/settings_subpages/profilepage.dart
+++ lib/pages/settings_subpages/profilepage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:swp_direktdem_verf_app/pages/settings_subpages/profilesettings.dart';
 import 'package:swp_direktdem_verf_app/pages/settings_subpages/usermodel.dart';
 import 'package:swp_direktdem_verf_app/widgets/custom_appbar.dart';
-import 'package:swp_direktdem_verf_app/widgets/profilewidget.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage(this.user, {Key? key}) : super(key: key);
@@ -20,16 +18,6 @@ class ProfilePage extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                ProfileWidget(
-                  imagePath: user.imagePath,
-                  onClicked: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => EditProfilePage(user),
-                      ),
-                    );
-                  },
-                ),
                 const SizedBox(height: 24),
                 buildName(user, context),
               ],
