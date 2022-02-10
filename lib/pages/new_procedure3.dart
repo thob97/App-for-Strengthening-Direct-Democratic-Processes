69c11b6f77b9eafa552a4c629ead2db24b2bf6c9
carlos93 <carlos93@mi.fu-berlin.de>
2022-02-10 20:07:58 +0000

    Update new_procedure3.dart

--- lib/pages/new_procedure3.dart
+++ lib/pages/new_procedure3.dart
@@ -34,12 +34,11 @@ class _NewProcedureState3 extends State<NewProcedure3> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        appBar: const CustomAppBar('Neues Verfahren erstellen'),
-        bottomNavigationBar:  Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: BottomAppBar(
+      appBar: const CustomAppBar('Neues Verfahren erstellen'),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: BottomAppBar(
           color: Colors.transparent,
           elevation: 0.0,
           child: Row(
@@ -49,144 +48,160 @@ class _NewProcedureState3 extends State<NewProcedure3> {
                 style: OutlinedButton.styleFrom(
                   padding: const EdgeInsets.symmetric(horizontal: 35),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const NewProcedure2(),),
+                      builder: (context) => const NewProcedure2(),
+                    ),
                   );
                 },
-                child: const Text('ZURÜCK',
+                child: const Text(
+                  'ZURÜCK',
                   style: TextStyle(
                     fontSize: 14,
                     letterSpacing: 2.2,
-                    color: Colors.black,),),
+                    color: Colors.black,
+                  ),
+                ),
               ),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const NewProcedure2(),),
+                      builder: (context) => const NewProcedure2(),
+                    ),
                   );
                 },
-
                 style: ElevatedButton.styleFrom(
                   primary: Colors.purple,
                   padding: const EdgeInsets.symmetric(horizontal: 30),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-
-
                 child: const Text(
                   'WEITER',
                   style: TextStyle(
                     fontSize: 14,
                     letterSpacing: 2.2,
-                    color: Colors.white,),
+                    color: Colors.white,
+                  ),
                 ),
               )
             ],
           ),
         ),
-        ),
-        body: Column(
-          children: [
-
-            SingleChildScrollView(
-                physics: const ScrollPhysics(),
-                child: Column(children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10, right: 10),
-                    child: Container(
-                      padding: const EdgeInsets.only(right: 10, top: 10),
-                      alignment: Alignment.topLeft,
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              bottom:
-                                  BorderSide(color: Colors.grey, width: 2.0),),),
-                      child: const Text('Datei hochladen',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(fontSize: 30),),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(
-                        left: 10, right: 10, top: 20, bottom: 30,),
+      ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            physics: const ScrollPhysics(),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Container(
+                    padding: const EdgeInsets.only(right: 10, top: 10),
                     alignment: Alignment.topLeft,
-                    child: const Text(
-                        'Hinweise: \nHier können Sie Dokumente zu Ihrem \nVolksbegehren hochladen. Dies können Sie auch\nspäter noch tun.',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontSize: 15),),
-                  ),
-
-
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: documentList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text(
-                            documentList.elementAt(index).documentName,),
-                        leading: const Icon(Icons.insert_drive_file_outlined),
-                        trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              const Icon(Icons.remove_red_eye),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.delete_outline,
-                                  size: 20.0,
-                                  color: Colors.white,
-                                ),
-                                onPressed: () {
-                                  //ScaffoldMessenger.of(context)
-                                  //    .showSnackBar(SnackBar(
-                                  //  content: Text("Call delete Listtile"),
-                                  //));
-                                  delete(index);
-                                  setState(() {});
-                                  //   _onDeleteItemPressed(index);
-                                },
-                              ),
-                            ],),
-                      );
-                    },
-                  ),
-
-
-                  TextButton.icon(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(color: Colors.blue),
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24.0),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.grey, width: 2.0),
                       ),
                     ),
-                    onPressed: () async {
-                      final result = await FilePicker.platform.pickFiles(
-                          type: FileType.custom, allowedExtensions: ['pdf'],);
-                      if (result == null) return;
-
-                      //open single file
-                      final file = result.files.first;
-
-                      documentList.add(Document(file.name, file.path.toString()));
-                      setState(() {});
-                    },
-                    icon: const Icon(
-                      Icons.add,
-                    ),
-                    label: const Text(
-                      'DOKUMENT HOCHLADEN',
+                    child: const Text(
+                      'Datei hochladen',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 30),
                     ),
                   ),
-                ],),),
-          ],
-        ),);
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                    top: 20,
+                    bottom: 30,
+                  ),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    'Hinweise: \nHier können Sie Dokumente zu Ihrem \nVolksbegehren hochladen. Dies können Sie auch\nspäter noch tun.',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: documentList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(
+                        documentList.elementAt(index).documentName,
+                      ),
+                      leading: const Icon(Icons.insert_drive_file_outlined),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Icon(Icons.remove_red_eye),
+                          IconButton(
+                            icon: const Icon(
+                              Icons.delete_outline,
+                              size: 20.0,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              //ScaffoldMessenger.of(context)
+                              //    .showSnackBar(SnackBar(
+                              //  content: Text("Call delete Listtile"),
+                              //));
+                              delete(index);
+                              setState(() {});
+                              //   _onDeleteItemPressed(index);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                TextButton.icon(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(color: Colors.blue),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final result = await FilePicker.platform.pickFiles(
+                      type: FileType.custom,
+                      allowedExtensions: ['pdf'],
+                    );
+                    if (result == null) return;
+
+                    //open single file
+                    final file = result.files.first;
+
+                    documentList.add(Document(file.name, file.path.toString()));
+                    setState(() {});
+                  },
+                  icon: const Icon(
+                    Icons.add,
+                  ),
+                  label: const Text(
+                    'DOKUMENT HOCHLADEN',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   void openFile(PlatformFile file) {
@@ -199,5 +214,4 @@ class Document {
 
   String documentName;
   String link;
-
 }
