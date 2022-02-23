cd8ff13d0d15f219266708b783d8b6b571aac794
thob97 <thob97@mi.fu-berlin.de>
2022-02-23 18:31:17 +0100

    feat[#83]: adapt my_procedures page to database service

--- lib/pages/settings_subpages/my_procedures.dart
+++ lib/pages/settings_subpages/my_procedures.dart
@@ -1,13 +1,92 @@
+import 'dart:io';
+
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:swp_direktdem_verf_app/mocked_data/mocked_selected_procedure_service.dart';
+import 'package:provider/provider.dart';
+import 'package:swp_direktdem_verf_app/methods/file_from_img_url_method.dart';
 import 'package:swp_direktdem_verf_app/pages/new_procedure.dart';
+import 'package:swp_direktdem_verf_app/pages/selected_procedure.dart';
+import 'package:swp_direktdem_verf_app/service/database_substitute.dart';
+import 'package:swp_direktdem_verf_app/service/model/procedure/simple_procedure.dart';
+import 'package:swp_direktdem_verf_app/service/service_database.dart';
+import 'package:swp_direktdem_verf_app/widgets/custom_future_builder.dart';
 import 'package:swp_direktdem_verf_app/widgets/home/foldable_filter_bar.dart';
 import 'package:swp_direktdem_verf_app/widgets/home/overview_procedure_view.dart';
 import 'package:swp_direktdem_verf_app/widgets/home/search_app_bar.dart';
 
-class MyProcedures extends StatelessWidget {
+class MyProcedures extends StatefulWidget {
   const MyProcedures();
+
+  ///Style
+  static const _placeholderMsg = 'Es wurden noch keine Verfahren erstellt';
+
+  @override
+  State<MyProcedures> createState() => _MyProceduresState();
+}
+
+class _MyProceduresState extends State<MyProcedures> {
+  late Future<List<ProcedureOverview>?> _procedures;
+
+  @override
+  void initState() {
+    super.initState();
+    _procedures = _getProcedures();
+  }
+
+  Future<List<SimpleProcedure>?> _getDataBaseData() async {
+    final String? ownUserId =
+        await Provider.of<ServiceDataBase>(context, listen: false).getOwnUser();
+    //for error handling
+    if (!mounted) return Future.delayed(Duration.zero, () => null);
+    //if not logged in, return null
+    return ownUserId == null
+        ? Future.delayed(Duration.zero, () => null)
+        : Provider.of<ServiceDataBase>(context, listen: false)
+            .getProceduresByEditor(ownUserId);
+  }
+
+  Future<List<ProcedureOverview>?> _getProcedures() async {
+    final List<SimpleProcedure>? _items = await _getDataBaseData();
+    if (_items == null) return null;
+
+    //for error handling
+    if (!mounted) return Future.delayed(Duration.zero, () => null);
+    final bool _isLoggedIn =
+        Provider.of<ServiceDataBase>(context, listen: false).userIsLoggedIn();
+
+    final List<ProcedureOverview> _res = [];
+    for (final SimpleProcedure _sP in _items) {
+      final File _imgFile = await fileFromImageUrl(
+        DatabaseSubstitute.getProcedureImg(procedureId: _sP.id),
+        _sP.id,
+      );
+      _res.add(
+        ProcedureOverview(
+          id: _sP.id,
+          title: _sP.title,
+          img: _imgFile,
+          subtitle: _sP.subtitle,
+          phaseState: _sP.lastCompletedPhase,
+          transitionItems: TransitionItems(
+            //TODO db: once error msg missing db gets resolved team
+            showEdit: _isLoggedIn,
+            //TODO db: once error msg missing db gets resolved team
+            isSubscribed: false,
+            //TODO db: once error msg missing db gets resolved team
+            showSubscribe: _isLoggedIn,
+            description: _sP.description,
+            process: _sP.process,
+            createDate: _sP.createDate,
+            creatorFirstName: '${_sP.carrier?.firstName}',
+            creatorLastName: '${_sP.carrier?.lastName}',
+            endDate: _sP.endDate,
+            organisationName: _sP.organisation?.name,
+          ),
+        ),
+      );
+    }
+    return _res;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +103,31 @@ class MyProcedures extends StatelessWidget {
             )
           ];
         },
-        body: OverviewProcedureView(
-          items: MockedSelectedProcedureService.getMyProcedures(),
+        body: RefreshIndicator(
+          onRefresh: _onRefresh,
+          child: body(context),
         ),
       ),
     );
+  }
+
+  Widget body(BuildContext context) {
+    return CustomFutureBuilder(
+      futureData: _procedures,
+      customErrorMsg: MyProcedures._placeholderMsg,
+      builder: (result) {
+        result as List<ProcedureOverview>;
+        return OverviewProcedureView(items: result);
+      },
+    );
+  }
+
+  Future<void> _onRefresh() async {
+    final List<ProcedureOverview>? _refreshedProcedures =
+        await _getProcedures();
+    setState(() {
+      _procedures = Future.delayed(Duration.zero, () => _refreshedProcedures);
+    });
   }
 
   //TODO
